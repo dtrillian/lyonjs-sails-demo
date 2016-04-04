104ec2335afaa340b7237601efb1600a9b117bcf
Malik Baba Aïssa <malik.mba@gmail.com>
2016-04-05 00:24:21 +0200

    Minor modification

--- config/twitter.js
+++ config/twitter.js
@@ -1,8 +1,6 @@
-var twitter = {
+module.exports.twitter = {
     consumer_key: 'xxxxx',
     consumer_secret: 'xxxxx',
     access_token_key: 'xxxxx',
     access_token_secret: 'xxxxx'
 };
-
-module.exports = twitter;
